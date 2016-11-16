37f5dc84947f483dd748d225c2651aa95c797c84
Samuel Diogo <samudiogo@gmail.com>
2016-11-15 22:13:20 -0200

    final commit release

---
+++ SdTech.FtpUploader/SdTech.FtpUploader.FtpUpload/FtpDestiny.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SdTech.FtpUploader.FtpUpload
+{
+    public class FtpDestiny
+    {
+
+        private string pEndereco;
+        public string PEndereco
+        {
+            get { return pEndereco; }
+            set
+            {
+                // TODO Test if already have a ftp
+                if (string.IsNullOrWhiteSpace(value)) throw new Exception("empty uri");
+                value = SanitizeEndereco(value);
+                if (!Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute)) throw new UriFormatException();
+                pEndereco = value;
+            }
+        }
+
+        private static string SanitizeEndereco(string end)
+        {
+            end = end.TrimEnd('/');
+            end = Regex.Replace(end, "^(https|http)", "ftp");
+            if (!Regex.Match(end, "^(ftp)://.*$").Success) return $"ftp://{end}";
+            return end;
+
+        }
+
+        private string pUsuario;
+        public string PUsuario
+        {
+            get { return pUsuario; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) pUsuario = value;
+                else throw new Exception("empty user");
+
+            }
+        }
+        private string pSenha;
+        public string PSenha
+        {
+            get { return pSenha; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) pSenha = value;
+                else throw new Exception("empty password");
+            }
+        }
+
+        private string pEnviar;
+        public string PEnviar
+        {
+            get { return pEnviar; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) pEnviar = value;
+                else throw new Exception("empty pEnviar");
+            }
+        }
+
+        public override string ToString() => $"End: {PEndereco}\nUsuario: {PUsuario}\nSenha: {PSenha}\nEnviar: {PEnviar}";
+    }
+}
--- SdTech.FtpUploader/SdTech.FtpUploader.FtpUpload/Program.cs
+++ SdTech.FtpUploader/SdTech.FtpUploader.FtpUpload/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Net;
 using static System.Text.Encoding;
 using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace SdTech.FtpUploader.FtpUpload
 {
@@ -23,27 +22,28 @@ namespace SdTech.FtpUploader.FtpUpload
 
                 Console.WriteLine(ftpParams);
 
-                var request = (FtpWebRequest)WebRequest.Create(ftpParams.PEndereco);
+                var remoteFile = $@"{ftpParams.PEndereco}/{ftpParams.PEnviar.Substring(ftpParams.PEnviar.LastIndexOf(@"\") + 1)}";
+
+                var request = (FtpWebRequest)WebRequest.Create(remoteFile);
                 request.Method = WebRequestMethods.Ftp.UploadFile;
                 request.Credentials = new NetworkCredential(ftpParams.PUsuario, ftpParams.PSenha);
+                request.UseBinary = true;
+                request.UsePassive = true;
+                request.KeepAlive = true;
 
                 byte[] fileContent = null;
                 using (var strReader = new StreamReader(ftpParams.PEnviar))
                 {
                     fileContent = UTF8.GetBytes(strReader.ReadToEnd());
                 }
-
                 request.ContentLength = fileContent.Length;
 
                 using (var requestStream = request.GetRequestStream())
                 {
                     requestStream.Write(fileContent, 0, fileContent.Length);
                 }
-
                 var response = (FtpWebResponse)request.GetResponse();
-
-                Console.WriteLine($"Upload file complete, status {response.StatusDescription}");
-
+                
             }
 
             catch (UriFormatException uriExcepion)
@@ -57,59 +57,8 @@ namespace SdTech.FtpUploader.FtpUpload
 
                 LogError.Log(ex, args);
             }
+
         }
-    }
-
-    public class FtpDestiny
-    {
-        private string pEndereco;
-        public string PEndereco
-        {
-            get { return pEndereco; }
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value)) throw new Exception("empty uri");
-                if (!Regex.Match(value, "^(https?|ftp)://.*$").Success) value = $"ftp://{value.TrimEnd('/')}";
-                if (!Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute)) throw new UriFormatException();
-                pEndereco = value;
-
-            }
-        }
-
-        private string pUsuario;
-        public string PUsuario
-        {
-            get { return pUsuario; }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value)) pUsuario = value;
-                else throw new Exception("empty user");
-
-            }
-        }
-        private string pSenha;
-        public string PSenha
-        {
-            get { return pSenha; }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value)) pSenha = value;
-                else throw new Exception("empty password");
-            }
-        }
-
-        private string pEnviar;
-        public string PEnviar
-        {
-            get { return pEnviar; }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value)) pEnviar = value;
-                else throw new Exception("empty pEnviar");
-            }
-        }
-
-        public override string ToString() => $"End: {PEndereco}\nUsuario: {PUsuario}\nSenha: {PSenha}\nEnviar: {PEnviar}";
     }
 
     public class LogError
